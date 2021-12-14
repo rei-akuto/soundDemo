37768e41f4bb6d42f3afc95dee93aae54f2ef35d
rei-akuto <akutoyuuki@gmail.com>
2021-12-15 00:04:23 +0900

    3d等追加

---
+++ Assets/HTC.UnityPlugin/Vive3DSP/Editor/Vive3DSPWindow.cs
@@ -0,0 +1,22 @@
+﻿//====================== Copyright 2017-2021, HTC.Corporation. All rights reserved. ======================
+/**
+*   release version:    1.2.6.0
+*   script version:     1.2.6.0
+*/
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace HTC.UnityPlugin.Vive3DSP
+{
+    public class Vive3DSPWindow : EditorWindow
+    {
+        [MenuItem("Window/VIVE 3DSP Audio SDK/Visit VIVE 3DSP Forum")]
+        public static void OpenVive3DSPForumWebSite()
+        {
+            Application.OpenURL(Vive3DSPAudio.VIVE_3DSP_FORUM_URL);
+        }
+    }
+}
